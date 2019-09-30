aec9b85cbd2529226607992169fa96538dace1fa
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-30 14:41:47 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,8 +4,8 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [:benvolio => {name: "Benolio", age: "17", status: "worried"},
-                        
+      :hero_friends => [:benvolio => {name: "Benolio", age: "17", attitude: "worried"},
+                        :mercutio => {name: "Mercutio", age:"18", attitude: "hot-headed"}
         #your hashes here!
       ]
    }, 
@@ -13,7 +13,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [:steven => {name: "Steven", age: "30", attitude: "confused"},}
         #your hashes here!
       ]
    }
