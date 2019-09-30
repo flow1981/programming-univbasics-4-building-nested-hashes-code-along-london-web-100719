a20f9829fe49e95a6107098ed2e40d730f24eb6b
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-09-30 14:36:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,33 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name:"Lord Mantague",
-        :age:"53"
+        name:"Lord Mantague",
+        age:"53"
         },
       :matriarch => {
-        :name: "Lady Mantaague",
-        :age: "54"
+        name: "Lady Mantaague",
+        age: "54"
         },
       :hero => {
-        :name: "Romeo",
-        :age: "15",
-        :status: "alive"
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         }
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name: "Lord Capulet",
-        :age: "50"
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
-        :name: "Lady Capulet",
-        :age: "51"
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
-        :name: "Juliet",
-        :age: "15",
-        :status: "alive"
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
